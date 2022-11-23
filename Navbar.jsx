e86d6d36ffb55db5826fb9e26eaf1630cfd4e3c6
Code With Z <71726246+codewithz@users.noreply.github.com>
2022-11-23 12:26:21 +0530

    Update Navbar.jsx

--- Navbar.jsx
+++ Navbar.jsx
@@ -5,7 +5,7 @@ export default function Navbar() {
     return (
 
         <nav class="navbar navbar-expand-lg navbar-dark bg-success">
-            <Link class="navbar-brand" to="/movies">Vidly</Link>
+            <Link className="navbar-brand" to="/movies">Vidly</Link>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
