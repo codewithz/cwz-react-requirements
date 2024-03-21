c491047734b62173daef6ec517e589ed8c96b410
Code With Z <71726246+codewithz@users.noreply.github.com>
2024-03-21 12:14:41 +0530

    Create renderSelectAndrenderButton.jsx

---
+++ renderSelectAndrenderButton.jsx
@@ -0,0 +1,42 @@
+export const renderSelect = (
+  label,
+  id,
+  options,
+  schema,
+  data,
+  setData,
+  errors,
+  setErrors
+) => {
+  return (
+    <div className="form-group mt-3">
+      <label htmlFor={id}>{label}</label>
+      <select
+        onChange={(e) =>
+          handleChange(e, schema, data, setData, errors, setErrors)
+        }
+        value={data.genreId}
+        id={id}
+        className="form-control mt-2"
+      >
+        <option value="" />
+        {options.map((option) => (
+          <option key={option._id} value={option._id}>
+            {option.name}
+          </option>
+        ))}
+      </select>
+      {errors[id] && <div className="alert alert-danger">{errors[id]}</div>}
+    </div>
+  );
+};
+
+export const renderButton = (buttonText, data, schema) => {
+  return (
+    <div className="mt-3">
+      <button disabled={validate(data, schema)} className="btn btn-primary">
+        {buttonText}
+      </button>
+    </div>
+  );
+};
